1d5c229d25b6c5552aeca1fafb06a61996076982
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2021-05-04 13:15:47 +0100

    Drop bootstrap_build.sh

--- autogen.sh
+++ autogen.sh
@@ -4,3 +4,4 @@ rm -rf build-aux/
 mkdir build-aux/
 cp libopencm3.mk build-aux/
 autoreconf --verbose --force --install --warnings=error
+./eglib/autogen.sh
--- bootstrap_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-./eglib/autogen.sh
-mkdir -p _build/
-cd _build/
-../autogen.sh
-../configure \
-	--host=arm-none-eabi
-make
